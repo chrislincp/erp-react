36a26ef7fa9f187686dfa35ab7d18e2ebc17280c
林昌鹏 <chrislincp@163.com>
2018-07-30 14:36:09 +0800

    路由引入方式

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from '../views/dashboard';
-import Test from '../views/test';
+import routes from './routes';
+
 export default class Routers extends React.Component {
+
     render() {
         return (
             <HashRouter>
                 <Switch>
                     <Route path="/admin/dashboard" component={Dashboard}  />
-                    <Route path="/admin/test" component={Test} />
+                    {routes.map(route => (
+                        <Route path={route.path} component={route.component} key={route.path} />
+                    ))}
                     <Route render={() => <Redirect to="/admin/dashboard" />} />
                 </Switch>
             </HashRouter>
