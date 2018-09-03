1186efaf1a11a72c5c21869b5cba030628298e8e
林昌鹏 <chrislincp@163.com>
2018-09-03 14:28:52 +0800

    add rematch replace redux

---
+++ src/store/models/user.js
@@ -0,0 +1,18 @@
+
+const user = {
+  state: {
+    userInfo: {},
+  },
+  reducers: {
+    setUserInfo(state, userInfo) {
+      return {
+        userInfo,
+      }
+    }
+  },
+  effects: {
+
+  }
+}
+
+export default user;
